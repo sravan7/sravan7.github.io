280684a80cbc7131812b13b024153e6aa1bc0d24
sravan7 <esd14i017@iiitdm.ac.in>
2018-12-05 16:53:06 +0530

    neighborhoods

---
+++ neighborhood/precache-manifest.aeb57c2433982d4c19e8a1e0fe1c5c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27cda1b76af547736888",
+    "url": "/static/js/main.27cda1b7.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "27cda1b76af547736888",
+    "url": "/static/css/main.d6a25cb2.chunk.css"
+  },
+  {
+    "revision": "22ed9e372be08c7b8e31ab4c0ee88e62",
+    "url": "/index.html"
+  }
+];
